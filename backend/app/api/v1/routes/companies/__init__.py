233baa37165dae2e7452c3874487b42d1b592483
henrysalkever <henry.salkever@icloud.com>
2025-02-22 18:30:18 +0000

    added routes

--- backend/app/api/v1/routes/companies/__init__.py
+++ backend/app/api/v1/routes/companies/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter 
 from .companies import router as companies_router 
-from .breach_type import router as breach_types_router 
+
 
 router = APIRouter()
 router.include_router(companies_router)
-router.include_router(breach_types_router,prefix="/companies")
